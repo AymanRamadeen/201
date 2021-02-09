524b644a11dc1ac81ffc51d78b7fdeb381f406fe
AymanRamadeen <ayman.abd2288@gmail.com>
2021-02-10 00:57:04 +0200

    files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+  let userName=prompt('enter your name');
+  alert('hello '+ userName+','+ 'my name is Ayman');
+  let wRite="hello "+ userName+","+"how are you?";
+  let age=prompt('what is your age ?')
+  alert ("your age is"+age)
+  
+  
+  document.write(wRite);
+  
